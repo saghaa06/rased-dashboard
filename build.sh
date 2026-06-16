3410425ec6d772c2a138d9893dc94085a42416a4
saghaa06 <148466439+saghaa06@users.noreply.github.com>
2026-06-16 04:17:21 +0100

    Auto-create superuser admin/adminpassword

--- build.sh
+++ build.sh
@@ -8,4 +8,15 @@ python manage.py collectstatic --noinput
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Creating superuser (admin/adminpassword)..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+EOF
+
 echo "Build completed!"
